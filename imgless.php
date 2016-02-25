a5affcf93c59d0d350c9813f7c8fd4138be7c5b2
Kalan Brock <kalanbrock@gmail.com>
2016-02-25 09:04:43 -0500

    Removed unused php stuff.

--- imgless.php
+++ imgless.php
@@ -1,7 +1,6 @@
 <?php
 
 $json = json_decode(file_get_contents('images.json'), true);
-
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 
@@ -13,9 +12,4 @@ if(isset($request->path) && isset($request->uri)){
 
   //delete the file, we no longer need it.
   //unlink($request->path);
-}
-
-//Get Data URI for saved image.
-if(isset($request->existingpath)){
-    echo (isset($json[$request->existingpath])) ? json_encode(['response' => json_encode(['uri' => $json[$request->existingpath]])]) : json_encode(['response' => json_encode(['uri' => 'false'])]);
 }
